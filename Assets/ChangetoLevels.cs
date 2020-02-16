055587faf636142552fa195d48296773a9312999
Sydney Keating <bookmasterdc@gmail.com>
2020-02-15 22:29:29 -0500

    Assets/Backup

---
+++ Assets/ChangetoLevels.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ChangetoLevels : MonoBehaviour
+{
+  public Button yourButton;
+
+	void Start () {
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void TaskOnClick(){
+	  SceneManager.LoadScene("LevelSelection");
+	}
+}
