3998db0d8df50f2ac92bcb73a7922b86f1d4ed23
Sydney Keating <bookmasterdc@gmail.com>
2020-02-29 22:52:57 -0500

    Updates

---
+++ Assets/OutfitAnimation.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OutfitAnimation : MonoBehaviour
+{
+    public Animator anim;
+     public string animname;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playanim();
+        if(Outfits.baseoutfit){
+           animname = "BunnyIdle";
+        }
+         else if(Outfits.outfitone){
+          animname = "BunnyOutfit1Idle";
+        }
+          else if(Outfits.outfittwo){
+          animname = "BunnyOutfit2Idle";
+        }
+    }
+public void playanim(){
+        anim.Play(animname);
+    }
+}
